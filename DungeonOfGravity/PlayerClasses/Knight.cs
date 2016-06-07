aace04b7bad494a7d4e46a42234f28bc2daebad0
johan haak <johanhaaken@gmail.com>
2016-06-07 12:11:40 +0200

    Start Strukture

---
+++ DungeonOfGravity/PlayerClasses/Knight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonOfGravity.PlayerClasses
+{
+    //Tank or block, extra health 
+    class Knight
+    {
+    }
+}
---
+++ DungeonOfGravity/PlayerClasses/Rookie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonOfGravity.PlayerClasses
+{
+    //start class, no extra stats
+    class Rookie
+    {
+    }
+}
